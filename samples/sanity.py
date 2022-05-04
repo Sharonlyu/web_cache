e96c6a371693d5636690e9684c292b682d743418
sharonliuyu <sharonliuyu@gmail.com>
2022-05-03 23:04:35 -0700

    first commit

---
+++ samples/sanity.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import string, re, random
+
+def allow_range_of_thread_counts(output):
+    normalized_lines = []
+    parallel = False
+    for line in output.split('\n'):
+        match = re.match(r'Number of requests: (\d+)', line)
+        if match:
+            num_requests = int(match.group(1))
+            if num_requests in [14, 15, 16, 17, 18]:
+                line = 'Number of parallel requests: 16 (+/- 2)'
+                parallel = True
+        match = re.match(r'Waiting requests released: (\d+)', line)
+        if match:
+            num_released = int(match.group(1))
+            if parallel and num_released in [12, 13, 14, 15, 16]:
+                line = 'All requests have been released.'
+        normalized_lines.append(line)
+    return '\n'.join(normalized_lines)
+
+def allow_any_myth_ip_address(output):
+    normalized_lines = []
+    for line in output.split('\n'):
+        line = re.sub('\[,171', '[171', line)
+        #if line.find('x-forwarded-for') >= 0: print(line)
+        line = re.sub('\[ ,171', '[171', line)
+        #if line.find('x-forwarded-for') >= 0: print(line)
+        line = re.sub(r'171\.64\.15\.\d{1,3}', '171.64.15.xyz', line)
+        #if line.find('x-forwarded-for') >= 0: print(line)                
+        normalized_lines.append(line)
+    return '\n'.join(normalized_lines)
